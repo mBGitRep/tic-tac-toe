2176217df1404a89501292d99f8e9f772dd08a9e
mirzabaig <baig.zee@hotmail.com>
2023-04-21 15:06:26 +0930

    code almost working fine but in progress

--- ticTacToe.js
+++ ticTacToe.js
@@ -19,4 +19,63 @@ startGame();
 
 function startGame(){
     box.forEach(cell => cell.addEventListener("click", cellClicked));
-    restBtn.addEventListener("click", restartGame)}
+    restBtn.addEventListener("click", restartGame);
+    statusCurrent.textContent = `${aPlayer}'s turn`;
+    working = true;
+}
+function cellClicked(){
+    var cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !working){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = aPlayer;
+    cell.textContent = aPlayer;
+}
+function chPlayer(){
+    aPlayer = (aPlayer == "X") ? "O" : "X";
+    statusCurrent.textContent = `${aPlayer}'s turn`;
+}
+function checkWinner(){
+    var roundWon = false;
+
+    for(let i = 0; i < winCombo.length; i++){
+        var condition = winCombo[i];
+        var cellA = options[condition[0]];
+        var cellB = options[condition[1]];
+        var cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusCurrent.textContent = `${aPlayer} wins!`;
+        working = false;
+    }
+    else if(!options.includes("")){
+        statusCurrent.textContent = `Draw!`;
+        working = false;
+    }
+    else{
+        chPlayer();
+    }
+}
+
+function restartGame(){
+    aPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusCurrent.textContent = `${aPlayer}'s turn`;
+    box.forEach(cell => cell.textContent = "");
+    working = true;
+}
