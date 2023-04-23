855b577fbcb0052f17b9d82386e971a0b25156af
mirzabaig <baig.zee@hotmail.com>
2023-04-24 08:52:46 +0930

    final day's morning push

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,81 +1,215 @@
-var box = document.querySelectorAll(".cell");
-var statusCurrent = document.querySelector("#statusCurrent");
-var restBtn = document.querySelector("#rBtn");
-var winCombo = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-var options = ["", "", "", "", "", "", "", "", ""];
-var aPlayer = "X";
-var working = false;
+var chance = "Player 1"
+var player = chance + "'s chance"
+var disabledBtn = 0
 
-startGame();
+document.querySelector("#chance").textContent = player
+function check() {
+    var btn1 = document.querySelector("#btn1").textContent
+    //document.querySelector('#btn1').addEventListener("click", function() {
+      //  disableBtn("btn1");
+    //});
+    var btn2 = document.querySelector("#btn2").textContent
+    var btn3 = document.querySelector("#btn3").textContent
+    var btn4 = document.querySelector("#btn4").textContent
+    var btn5 = document.querySelector("#btn5").textContent
+    var btn6 = document.querySelector("#btn6").textContent
+    var btn7 = document.querySelector("#btn7").textContent
+    var btn8 = document.querySelector("#btn8").textContent
+    var btn9 = document.querySelector("#btn9").textContent
+    var result = document.querySelector("#resultText")
+    var reset = document.querySelector("#reset-btn")
+    
+   /*document.querySelector('btn1').addEventListener('click', function() {
+        disableBtn('btn1');
+        });
+        document.querySelector('btn2').addEventListener('click', function() {
+        disableBtn('btn2');
+        });
+        document.querySelector('btn3').addEventListener('click', function() {
+        disableBtn('btn3');
+        });
+        document.querySelector('btn4').addEventListener('click', function() {
+        disableBtn('btn4');
+        });
+        document.querySelector('btn5').addEventListener('click', function() {
+        disableBtn('btn5');
+        });
+        document.querySelector('btn6').addEventListener('click', function() {
+        disableBtn('btn6');
+        });
+        document.querySelector('btn7').addEventListener('click', function() {
+        disableBtn('btn7');
+        });
+        document.querySelector('btn8').addEventListener('click', function() {
+        disableBtn('btn8');
+        });
+        document.querySelector('btn9').addEventListener('click', function() {
+        disableBtn('btn9');
+        });
+        */
+        
 
-function startGame(){
-    box.forEach(cell => cell.addEventListener("click", cellClicked));
-    restBtn.addEventListener("click", restartGame);
-    statusCurrent.textContent = `${aPlayer}'s turn`;
-    working = true;
-}
-function cellClicked(){
-    var cellIndex = this.getAttribute("cellIndex");
-
-    if(options[cellIndex] != "" || !working){
-        return;
+    if (btn1 === "x" && btn2 === "x" && btn3 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
     }
 
-    updateCell(this, cellIndex);
-    checkWinner();
-}
-function updateCell(cell, index){
-    options[index] = aPlayer;
-    cell.textContent = aPlayer;
-}
-function chPlayer(){
-    aPlayer = (aPlayer == "X") ? "O" : "X";
-    statusCurrent.textContent = `${aPlayer}'s turn`;
-}
-function checkWinner(){
-    var roundWon = false;
-
-    for(let i = 0; i < winCombo.length; i++){
-        var condition = winCombo[i];
-        var cellA = options[condition[0]];
-        var cellB = options[condition[1]];
-        var cellC = options[condition[2]];
-
-        if(cellA == "" || cellB == "" || cellC == ""){
-            continue;
-        }
-        if(cellA == cellB && cellB == cellC){
-            roundWon = true;
-            break;
-        }
+    else if (btn4 === "x" && btn5 === "x" && btn6 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
     }
 
-    if(roundWon){
-        statusCurrent.textContent = `${aPlayer} wins!`;
-        working = false;
+    else if (btn7 === "x" && btn8 === "x" && btn9 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
     }
-    else if(!options.includes("")){
-        statusCurrent.textContent = `Draw!`;
-        working = false;
+
+    else if (btn1 === "x" && btn4 === "x" && btn7 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
     }
-    else{
-        chPlayer();
+
+    else if (btn2 === "x" && btn5 == "x" && btn8 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn3 === "x" && btn6 === "x" && btn9 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn1 === "x" && btn5 === "x" && btn9 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn3 === "x" && btn5 === "x" && btn7 === "x") {
+        disable()
+        result.textContent = "Player 1 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn1 === "o" && btn2 === "o" && btn3 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn4 === "o" && btn5 === "o" && btn6 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn7 === "o" && btn8 === "o" && btn9 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn1 === "o" && btn4 === "o" && btn7 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn2 === "o" && btn5 == "o" && btn8 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn3 === "o" && btn6 === "o" && btn9 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn1 === "o" && btn5 === "o" && btn9 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (btn3 === "o" && btn5 === "o" && btn7 === "o") {
+        disable()
+        result.textContent = "Player 2 won !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+    else if (disabledBtn === 9) {
+        disable()
+        result.textContent = "Match Draw !"
+        result.style.visibility = "visible"
+        reset.style.visibility = "visible"
+    }
+
+}
+
+function disableBtn(id) {
+    if (chance === "Player 1") {
+        document.getElementById(id).disabled = true
+        document.getElementById(id).textContent = "x"
+        chance = "Player 2"
+        player = chance + "'s chance"
+        document.querySelector("#chance").textContent = player
+        disabledBtn = disabledBtn + 1
+        check()
+    }
+    else if (chance === "Player 2") {
+        document.getElementById(id).disabled = true
+        document.getElementById(id).textContent = "o"
+        chance = "Player 1"
+        player = chance + "'s chance"
+        document.querySelector("#chance").textContent = player
+        disabledBtn = disabledBtn + 1
+        check()
     }
 }
 
-function restartGame(){
-    aPlayer = "X";
-    options = ["", "", "", "", "", "", "", "", ""];
-    statusCurrent.textContent = `${aPlayer}'s turn`;
-    box.forEach(cell => cell.textContent = "");
-    working = true;
+function disable() {
+    for (var i = 1; i < 10; i++) {
+        var id = "btn" + i
+        document.getElementById(id).disabled = true
+    }
+    document.querySelector("#reset-btn").style.visibility = "visible"
+    document.querySelector("#resultText").style.visibility = "visible"
+}
+
+function reset() {
+    for (var i = 1; i < 10; i++) {
+        var id = "btn" + i
+        document.getElementById(id).disabled = false
+        document.getElementById(id).textContent = ""
+    }
+    chance = "Player 1"
+    player = chance + "'s chance"
+    document.querySelector("#chance").textContent = player
+    document.querySelector("#reset-btn").style.visibility = "hidden"
+    document.querySelector("#resultText").style.visibility = "hidden"
+    disabledBtn = 0
 }
